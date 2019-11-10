179f788190cd8f3b952ed022063d92c17e02434b
做全栈攻城狮 <iquanzhan@163.com>
2019-11-10 17:19:12 +0800

    Person测试环境搭建

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import com.chengxiaoxiao.bean.Person;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    private static final SessionFactory ourSessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            ourSessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+    public static void main(final String[] args) throws Exception {
+        final Session session = getSession();
+        try {
+
+            Transaction transaction = session.beginTransaction();
+
+            session.save(new Person(1,"bane",18,"中国"));
+            transaction.commit();
+        } finally {
+            session.close();
+        }
+    }
+}
